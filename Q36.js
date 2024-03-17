846b7c2b2a22a48f72fe97138e8872cb2bd37b14
Sheikh-Muhammad-Sadiq <162907621+Sheikh-Muhammad-Sadiq@users.noreply.github.com>
2024-03-17 04:18:07 -0700

    Add files via upload
    Q 36 Node.js of 45 Assignments

---
+++ Q36.js
@@ -0,0 +1,11 @@
+"use strict";
+// 36. T-Shirt: Write a function called make_shirt() that accepts a size and the
+// text of a message that should be printed on the shirt. The function should print
+// a sentence summarizing the size of the shirt and the message printed on it.
+// Call the function.
+function make_shirt(size, message) {
+    console.log(`I'm making a ${size} T.shirt with the message "${message}"`);
+}
+make_shirt("Small", "I love Typescript!");
+make_shirt("Medium", "I love Typescript!");
+make_shirt("Large", "I love Typescript!");
